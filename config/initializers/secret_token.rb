d7f2edcd46b27787ed44d342ba26ce1580f675fb
Chris Neitzer <chris@neitzer.com>
2012-03-03 14:11:41 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antrim::Application.config.secret_token = '72ae4d382d68a4b2f40ee15e02e1dba56b62ca4707c43f6b78019b8bffe6f5337da0ed4b7a13e13f9faa1b57f1d6614ae5e40b4b187ef74343d63e8c740815b6'
