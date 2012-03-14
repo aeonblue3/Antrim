c111d4c9496d61075bdd92da116b42be4080d4e4
Chris Neitzer <chrisn@soholaunch.com>
2012-03-14 17:00:43 -0400

    Added nested_form.js ro precompile list in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Antrim::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['admin/active_admin.js', 'admin/active_admin.css']
+  config.assets.precompile += ['admin/active_admin.js', 'admin/active_admin.css', 'admin/nested_form.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
